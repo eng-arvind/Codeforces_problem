4717f374e2fda556bd2fb80b1424ca7743f8654f
Arvind kumar Vishwakarma <41833207+eng-arvind@users.noreply.github.com>
2020-08-25 21:33:59 +0530

    Add files via upload

---
+++ String_Similarity.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include<bitset>
+#include<string>
+#include<sstream>
+#include <algorithm>
+#define pb push_back
+#define s second
+#define f first
+#define mp make_pair
+#define pii pair<int,int>
+#define vi vector<int>
+#define mii map<int,int>
+#define setbits(x) __builtin_popcountll(x)
+#define mod 1000000007
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define ll long long int
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+      int n;
+      cin>>n;
+      string s;
+      cin>>s;
+      string ch;
+      ll index=0;
+      for(int i=0;i<n;i++)
+      {
+        ch+=s[i+index];
+        index++;
+      }
+      cout<<ch<<endl;
+    }
+    return 0;
+  }
